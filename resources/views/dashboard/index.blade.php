9a3abb92d64e103c0981115e9c2a605fd29ed932
Oskar <oskarguz.g@gmail.com>
2023-04-01 16:44:55 +0200

    clean up code

---
+++ resources/views/dashboard/index.blade.php
@@ -0,0 +1,3 @@
+<x-app-layout>
+    <p>Hello on dashboard page!</p>
+</x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', HomeController::class)->name('home');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/dashboard', DashboardController::class)->name('dashboard');
 });
